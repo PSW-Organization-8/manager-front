5ea82c1847f07a2435bde87df2fb470ac7b6d677
Filip Pinjuh <74602296+piwneuh@users.noreply.github.com>
2021-11-25 17:03:43 +0100

    feat: added tooltips to graphs (#8)

--- manager-application/src/app/survey-data/survey-data.component.ts
+++ manager-application/src/app/survey-data/survey-data.component.ts
@@ -12,25 +12,43 @@ export class SurveyDataComponent implements OnInit {
   dataSourceCategoryChart: Object = empty;
   questionValues: any;
   categoryValues: any;
+  questionData: any;
+  categoryData: any;
 
   constructor(private _allSurveyDataService: AllSurveyDataService) {}
 
   ngOnInit(): void {   
-    this.getQuestionAvgValuesFromServer()
-    this.getCategoryAvgValuesFromServer()
+    this.getQuestionAvgValues()
+    this.getCategoryAvgValues()
   }
 
-  getQuestionAvgValuesFromServer(): void{
+  getQuestionAvgValues(): void{
     this._allSurveyDataService.getQuestionAvgValuesFromServer().subscribe(
       successData => {  this.questionValues = successData },
+      error => { },
+      () => { this.getQuestionData() }
+      );
+  }
+
+  getCategoryAvgValues(): void{
+    this._allSurveyDataService.getCategoryAvgValuesFromServer().subscribe(
+      successData => {  this.categoryValues = successData },
+      error => { },
+      () => { this.getCategoryData() }
+      );
+  }
+
+  getQuestionData(): void{
+    this._allSurveyDataService.getQuesitonDataFromServer().subscribe(
+      successData => {  this.questionData = successData },
       error => { },
       () => { this.chartQuestionValues() }
       );
   }
 
-  getCategoryAvgValuesFromServer(): void{
-    this._allSurveyDataService.getCategoryAvgValuesFromServer().subscribe(
-      successData => {  this.categoryValues = successData },
+  getCategoryData(): void{
+    this._allSurveyDataService.getCategoryDataFromServer().subscribe(
+      successData => {  this.categoryData = successData },
       error => { },
       () => { this.chartCategoryValues() }
       );
@@ -47,9 +65,9 @@ export class SurveyDataComponent implements OnInit {
         theme: 'fusion'
       },
       data: [
-        { label: '1.', value: this.categoryValues[0] },
-        { label: '2.', value: this.categoryValues[1] },
-        { label: '3.', value: this.categoryValues[2] }
+        { label: '1.', value: this.categoryValues[0], tooltext: 'Text: About Doctor' + '<br> Avg: ' + parseFloat(this.categoryValues[0].toFixed(2)) + '<br> Num of 1:  ' + this.categoryData[0].ones + '<br> Num of 2: ' + this.categoryData[0].twos + '<br> Num of 3: ' + this.categoryData[0].threes + '<br> Num of 4: ' + this.categoryData[0].fours + '<br> Num of 5: ' + this.categoryData[0].fives },
+        { label: '2.', value: this.categoryValues[1], tooltext: 'Text: About Hospital' + '<br> Avg: ' + parseFloat(this.categoryValues[1].toFixed(2)) + '<br> Num of 1:  ' + this.categoryData[1].ones + '<br> Num of 2: ' + this.categoryData[1].twos + '<br> Num of 3: ' + this.categoryData[1].threes + '<br> Num of 4: ' + this.categoryData[1].fours + '<br> Num of 5: ' + this.categoryData[1].fives },
+        { label: '3.', value: this.categoryValues[2], tooltext: 'Text: About Medical Staff' + '<br> Avg: ' + parseFloat(this.categoryValues[2 ].toFixed(2)) + '<br> Num of 1:  ' + this.categoryData[2].ones + '<br> Num of 2: ' + this.categoryData[2].twos + '<br> Num of 3: ' + this.categoryData[2].threes + '<br> Num of 4: ' + this.categoryData[2].fours + '<br> Num of 5: ' + this.categoryData[2].fives }
       ],
       trendlines: [
         {
@@ -57,7 +75,7 @@ export class SurveyDataComponent implements OnInit {
             {
               startvalue: this.avg(this.categoryValues),
               valueOnRight: 1,
-              displayValue: 'All Question Avg',
+              displayValue: 'All Question Avg: ' + parseFloat(this.avg(this.categoryValues).toFixed(2)).toString(),
               thickness: 3,
               color: "#efb0a1"
             }
@@ -78,21 +96,21 @@ export class SurveyDataComponent implements OnInit {
         theme: 'fusion',
       },
       data: [
-        { label: '1.', value: this.questionValues[0] },
-        { label: '2.', value: this.questionValues[1] },
-        { label: '3.', value: this.questionValues[2] },
-        { label: '4.', value: this.questionValues[3] },
-        { label: '5.', value: this.questionValues[4] },
-        { label: '6.', value: this.questionValues[5] },
-        { label: '7.', value: this.questionValues[6] },
-        { label: '8.', value: this.questionValues[7] },
-        { label: '9.', value:  this.questionValues[8] },
-        { label: '10.', value: this.questionValues[9] },
-        { label: '11.', value: this.questionValues[10] },
-        { label: '12.', value: this.questionValues[11] },
-        { label: '13.', value: this.questionValues[12] },
-        { label: '14.', value: this.questionValues[13] },
-        { label: '15.', value: this.questionValues[14] }
+        { label: '1.', value: this.questionValues[0], tooltext: 'Text: How would you rate doctors professionalism?' + '<br> Avg: ' + parseFloat(this.questionValues[0].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[0].ones + '<br> Num of 2: ' + this.questionData[0].twos + '<br> Num of 3: ' + this.questionData[0].threes + '<br> Num of 4: ' + this.questionData[0].fours + '<br> Num of 5: ' + this.questionData[0].fives},
+        { label: '2.', value: this.questionValues[1], tooltext: 'Text: How would you rate doctors politeness?' + '<br> Avg: ' + parseFloat(this.questionValues[1].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[1].ones + '<br> Num of 2: ' + this.questionData[1].twos + '<br> Num of 3: ' + this.questionData[1].threes + '<br> Num of 4: ' + this.questionData[1].fours + '<br> Num of 5: ' + this.questionData[1].fives },
+        { label: '3.', value: this.questionValues[2], tooltext: 'Text: How would you rate doctors technicality?' + '<br> Avg: ' + parseFloat(this.questionValues[2].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[2].ones + '<br> Num of 2: ' + this.questionData[2].twos + '<br> Num of 3: ' + this.questionData[2].threes + '<br> Num of 4: ' + this.questionData[2].fours + '<br> Num of 5: ' + this.questionData[2].fives },
+        { label: '4.', value: this.questionValues[3], tooltext: 'Text: How would you rate doctors skill?' + '<br> Avg: ' + parseFloat(this.questionValues[3].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[3].ones + '<br> Num of 2: ' + this.questionData[3].twos + '<br> Num of 3: ' + this.questionData[3].threes + '<br> Num of 4: ' + this.questionData[3].fours + '<br> Num of 5: ' + this.questionData[3].fives },
+        { label: '5.', value: this.questionValues[4], tooltext: 'Text: How would you rate doctors knowledge?' + '<br> Avg: ' + parseFloat(this.questionValues[4].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[4].ones + '<br> Num of 2: ' + this.questionData[4].twos + '<br> Num of 3: ' + this.questionData[4].threes + '<br> Num of 4: ' + this.questionData[4].fours + '<br> Num of 5: ' + this.questionData[4].fives },
+        { label: '6.', value: this.questionValues[5], tooltext: 'Text: How would you rate hospital environment?' + '<br> Avg: ' + parseFloat(this.questionValues[5].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[5].ones + '<br> Num of 2: ' + this.questionData[5].twos + '<br> Num of 3: ' + this.questionData[5].threes + '<br> Num of 4: ' + this.questionData[5].fours + '<br> Num of 5: ' + this.questionData[5].fives },
+        { label: '7.', value: this.questionValues[6], tooltext: 'Text: How would you rate hospital equipment?' + '<br> Avg: ' + parseFloat(this.questionValues[6].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[6].ones + '<br> Num of 2: ' + this.questionData[6].twos + '<br> Num of 3: ' + this.questionData[6].threes + '<br> Num of 4: ' + this.questionData[6].fours + '<br> Num of 5: ' + this.questionData[6].fives},
+        { label: '8.', value: this.questionValues[7], tooltext: 'Text: How would you rate hospital hygiene?' + '<br> Avg: ' + parseFloat(this.questionValues[7].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[7].ones + '<br> Num of 2: ' + this.questionData[7].twos + '<br> Num of 3: ' + this.questionData[7].threes + '<br> Num of 4: ' + this.questionData[7].fours + '<br> Num of 5: ' + this.questionData[7].fives},
+        { label: '9.', value:  this.questionValues[8], tooltext: 'Text: How would you rate hospital prices?' + '<br> Avg: ' + parseFloat(this.questionValues[8].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[8].ones + '<br> Num of 2: ' + this.questionData[8].twos + '<br> Num of 3: ' + this.questionData[8].threes + '<br> Num of 4: ' + this.questionData[8].fours + '<br> Num of 5: ' + this.questionData[8].fives},
+        { label: '10.', value: this.questionValues[9], tooltext: 'Text: How would you rate hospital waiting time?' + '<br> Avg: ' + parseFloat(this.questionValues[9].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[9].ones + '<br> Num of 2: ' + this.questionData[9].twos + '<br> Num of 3: ' + this.questionData[9].threes + '<br> Num of 4: ' + this.questionData[9].fours + '<br> Num of 5: ' + this.questionData[9].fives},
+        { label: '11.', value: this.questionValues[10], tooltext: 'Text: How would you rate medical staffs professionalism?' + '<br> Avg: ' + parseFloat(this.questionValues[10].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[10].ones + '<br> Num of 2: ' + this.questionData[10].twos + '<br> Num of 3: ' + this.questionData[10].threes + '<br> Num of 4: ' + this.questionData[10].fours + '<br> Num of 5: ' + this.questionData[10].fives },
+        { label: '12.', value: this.questionValues[11], tooltext: 'Text: How would you rate medical staffs politeness?' + '<br> Avg: ' + parseFloat(this.questionValues[11].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[11].ones + '<br> Num of 2: ' + this.questionData[11].twos + '<br> Num of 3: ' + this.questionData[11].threes + '<br> Num of 4: ' + this.questionData[11].fours + '<br> Num of 5: ' + this.questionData[11].fives},
+        { label: '13.', value: this.questionValues[12], tooltext: 'Text: How would you rate medical staffs technicality?' + '<br> Avg: ' + parseFloat(this.questionValues[12].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[12].ones + '<br> Num of 2: ' + this.questionData[12].twos + '<br> Num of 3: ' + this.questionData[12].threes + '<br> Num of 4: ' + this.questionData[12].fours + '<br> Num of 5: ' + this.questionData[12].fives },
+        { label: '14.', value: this.questionValues[13], tooltext: 'Text: How would you rate medical staffs skill?' + '<br> Avg: ' + parseFloat(this.questionValues[13].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[13].ones + '<br> Num of 2: ' + this.questionData[13].twos + '<br> Num of 3: ' + this.questionData[13].threes + '<br> Num of 4: ' + this.questionData[13].fours + '<br> Num of 5: ' + this.questionData[13].fives },
+        { label: '15.', value: this.questionValues[14], tooltext: 'Text: How would you rate medical staffs knowledge?' + '<br> Avg: ' + parseFloat(this.questionValues[14].toFixed(2)) + '<br> Num of 1:  ' + this.questionData[14].ones + '<br> Num of 2: ' + this.questionData[14].twos + '<br> Num of 3: ' + this.questionData[14].threes + '<br> Num of 4: ' + this.questionData[14].fours + '<br> Num of 5: ' + this.questionData[14].fives }
       ],
       trendlines: [
         {
@@ -100,7 +118,7 @@ export class SurveyDataComponent implements OnInit {
             {
               startvalue: this.avg(this.questionValues),
               valueOnRight: 1,
-              displayValue: 'All Question Avg',
+              displayValue: 'All Question Avg: ' + parseFloat(this.avg(this.questionValues).toFixed(2)).toString(),
               thickness: 3,
               color: "#efb0a1"
             }
