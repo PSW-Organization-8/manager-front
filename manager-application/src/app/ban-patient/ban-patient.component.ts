16b7f5aeec417ed44b21a7aa18e2123b4259607f
MitarBrankovic <brankovic.ra198.2018@uns.ac.rs>
2021-12-03 18:26:13 +0100

    feat: table done

--- manager-application/src/app/ban-patient/ban-patient.component.ts
+++ manager-application/src/app/ban-patient/ban-patient.component.ts
@@ -8,21 +8,33 @@ import {BanPatientService} from './ban-patient.service'
   styleUrls: ['./ban-patient.component.css']
 })
 export class BanPatientComponent implements OnInit {
-  patients: any;
-  numCanceled : number = 0;
+  dtos: any;
 
   constructor(private _banPatientService: BanPatientService) {}
 
   ngOnInit(): void {   
-    this.getAllPatients();
+
+    this.getDto();
   }
 
-  getAllPatients(): void{
-    this._banPatientService.getPatientsFromServer().subscribe(f => this.patients = f);
+  getDto(): void{
+    this._banPatientService.getDto().subscribe(f => this.dtos = f);
   }
 
-  getCanceled(id : number): void{
-    this._banPatientService.getNumOfCanceledFromServer(id).subscribe(f => this.numCanceled = f);
+  banPatient(patientId: number): void{
+    this._banPatientService.banPatient(patientId).subscribe(() => this.getDto());
+  }
+
+  unbanPatient(patientId: number): void{
+    this._banPatientService.unbanPatient(patientId).subscribe(() => this.getDto());
+  }
+
+  color(numCanceled: number): string{
+    if(numCanceled > 2)
+      return 'tomato'
+    else
+      return ''
+
   }
 
 }
--- manager-application/src/app/ban-patient/ban-patient.service.ts
+++ manager-application/src/app/ban-patient/ban-patient.service.ts
@@ -11,12 +11,15 @@ export class BanPatientService {
   private _url = hospitalServerPort;
   constructor(private http: HttpClient) { }
 
-  public getPatientsFromServer(): Observable<any> {
-    return this.http.get<any>(this._url + 'patient');
+  public getDto(): Observable<any> {
+    return this.http.get<any>(this._url + 'appointment');
   }
 
-  public getNumOfCanceledFromServer(patientId: number): Observable<any> {
-    return this.http.get<any>(this._url + 'appointment/cancel/' + patientId);
+  public banPatient(patientId: number) {
+    return this.http.put<string>(this._url + 'patient/ban/' + patientId, patientId);
+  }
+  public unbanPatient(patientId: number) {
+    return this.http.put<string>(this._url + 'patient/unban/' + patientId, patientId);
   }
 
 }
