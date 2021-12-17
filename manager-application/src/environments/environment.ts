3b51d6b4695c0b1ee9d5be1a360f5af54b9b295d
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-12-17 11:12:53 +0100

    feat: added env vars and fixed hrefs

--- manager-application/src/environments/environment.ts
+++ manager-application/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  integrationsAPI: 'http://localhost:7313/api/',
+  patientsAPI: 'http://localhost:16934/api/',
 };
 
 /*
