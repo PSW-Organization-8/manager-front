621175b85433e57f2c38506b8980769ff4130340
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2021-12-19 17:02:29 +0100

    feat: create, view, close tender
    feat: added components for tendering and table for all tenders
    
    fix: error proofing
    
    feat: showing all tenders
    
    feat: added modal view for tender details
    
    feat: closing tenders

---
+++ manager-application/src/app/tenders/tenders-view/tenders-view.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { TenderService } from '../tender.service';
+
+@Component({
+  selector: 'app-tenders-view',
+  templateUrl: './tenders-view.component.html',
+  styleUrls: ['./tenders-view.component.css']
+})
+export class TendersViewComponent implements OnInit {
+
+  constructor(private tenderService: TenderService,
+    private toastr: ToastrService
+    ) { }
+
+  tenders : any
+
+  ngOnInit(): void {
+    this.getAllTenders();
+  }
+
+  getAllTenders() {
+    this.tenderService.getAll().subscribe(
+      (data)=>{
+        this.tenders = data
+        this.setModals()
+      },
+      (error)=>{
+        this.toastr.error("There is an error")
+      }
+    )
+  }
+  setModals() {
+    for(let tender of this.tenders){
+      tender.isModalActive = false;
+    }
+  }
+
+  toggleModal(tender: any) {
+    tender.isModalActive = !tender.isModalActive;
+  }
+
+  canClose(endDateString: string){
+    if(endDateString == null || endDateString == ""){
+      return true
+    }
+
+    let dateTimeSplited = endDateString.split("T")
+    let date = dateTimeSplited[0].split("-")
+    let time = dateTimeSplited[1].split(":")
+    let dateTime = new Date(parseInt(date[0]), parseInt(date[1])-1, parseInt(date[2]), parseInt(time[0]), parseInt(time[1]), parseInt(time[2]))
+    
+    return dateTime >= new Date()
+  }
+
+  seeOffers(){
+
+  }
+
+  closeTender(tender: any){
+    this.tenderService.closeTender(tender.id).subscribe(
+      (data)=>{
+        this.getAllTenders();
+        this.toastr.success("Successfully closed tender")
+      },
+      (error)=>{
+        this.toastr.error("There is an error")
+      }
+    )
+  }
+  
+}
