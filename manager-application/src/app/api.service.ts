f30f15a717c8ef091fc33b00a65d175dd3aa775b
Violeta Kukic <vkukic99@gmail.com>
2022-01-24 09:01:23 +0100

    e2e tests

--- manager-application/src/app/api.service.ts
+++ manager-application/src/app/api.service.ts
@@ -13,70 +13,70 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getBuildings() {
-    return this.http.get(this.baseURL + '/api/building');
+    return this.http.get(this.baseURL + 'building');
   }
 
   getRooms() {
-    return this.http.get(this.baseURL + '/api/room');
+    return this.http.get(this.baseURL + 'room');
   }
 
   getFloors() {
-    return this.http.get(this.baseURL + '/api/floor');
+    return this.http.get(this.baseURL + 'floor');
   }
 
   getEquipments() {
-    return this.http.get(this.baseURL + '/api/equipment');
+    return this.http.get(this.baseURL + 'equipment');
   }
 
   getMoveEquipments() {
-    return this.http.get(this.baseURL + '/api/moveEquipment');
+    return this.http.get(this.baseURL + 'moveEquipment');
   }
   
   getRoom(id: any) {
-    return this.http.get(this.baseURL + '/api/room/' + id);
+    return this.http.get(this.baseURL + 'room/' + id);
   }
 
   
 
   getShift(id : any) {
-    return this.http.get(this.baseURL + "/api/shift/" + id);
+    return this.http.get(this.baseURL + "shift/" + id);
   }
   getVacation(id : any) {
-    return this.http.get(this.baseURL + "/api/vacation/" + id);
+    return this.http.get(this.baseURL + "vacation/" + id);
   }
 
   getDoctor(id : any) {
-    return this.http.get(this.baseURL + "/api/doctor/" + id);
+    return this.http.get(this.baseURL + "doctor/" + id);
   }
 
   editShift(data : any) {
-    return this.http.put(this.baseURL + "/api/shifts/edit", data);
+    return this.http.put(this.baseURL + "shifts/edit", data);
   }
 
   editDoctorShift(shiftID: any, doctorID: any) {
-    return this.http.put(this.baseURL + '/api/editDoctorShift/' + shiftID + '/' + doctorID, {});
+    return this.http.put(this.baseURL + 'editDoctorShift/' + shiftID + '/' + doctorID, {});
   }
   getShifts()
   {
-    return this.http.get(this.baseURL + "/api/shift");
+    return this.http.get(this.baseURL + "shift");
   }
 
   getVacations()
   {
-    return this.http.get(this.baseURL + "/api/vacation");
+    return this.http.get(this.baseURL + "vacation");
   }
   getDoctors()
   {
-    return this.http.get(this.baseURL + "/api/doctor/allDoctors");
+    return this.http.get(this.baseURL + "doctor/allDoctors");
   }
   getEquipment(id: any) {
-    return this.http.get(this.baseURL + '/api/equipment/' + id);
+    return this.http.get(this.baseURL + 'equipment/' + id);
   }
   getSearchTerm(id: any) {
-    return this.http.get(this.baseURL + '/api/search/' + id);
+    return this.http.get(this.baseURL + 'search/' + id);
   }
   postSubmitRelocation(data: any) {
-    return this.http.post(this.baseURL + '/api/submitRelocation' ,data);
+    return this.http.post(this.baseURL + 'submitRelocation' ,data);
   }
 
 
